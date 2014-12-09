93132183f5fa9b104f6940ae2569054fb261a110
tedliang <nightbear1009@gmail.com>
2014-12-10 00:15:17 +0800

    use scrollY to scroll

--- ParallelListView/app/src/main/java/com/example/ted/parallellistview/MyActivity.java
+++ ParallelListView/app/src/main/java/com/example/ted/parallellistview/MyActivity.java
@@ -17,6 +17,9 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.greenrobot.event.EventBus;
 
 
@@ -24,17 +27,17 @@ public class MyActivity extends Activity {
     private ListView mListView;
     private MyAdapter mAdapter;
 
-    public static class MyAdapter extends BaseAdapter{
+    public static class MyAdapter extends BaseAdapter {
         private LayoutInflater inflater;
-        private Bitmap mBitmap;
-        public MyAdapter(Context context,Bitmap bitmap){
+
+        public MyAdapter(Context context) {
             inflater = LayoutInflater.from(context);
-            mBitmap = bitmap;
-            Log.d("Ted","init");
+            Log.d("Ted", "init");
         }
+
         @Override
         public int getCount() {
-            return 300;
+            return Data.URLS.length*10;
         }
 
         @Override
@@ -49,35 +52,34 @@ public class MyActivity extends Activity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            Log.d("Ted","getView");
+            Log.d("Ted", "getView");
             ViewHolder holder;
-            if(view == null){
+            if (view == null) {
 
                 holder = new ViewHolder();
-                view = inflater.inflate(R.layout.adapter_layout,null,false);
-                holder.textview = (TextView)view.findViewById(R.id.textview);
+                view = inflater.inflate(R.layout.adapter_layout, null, false);
+                holder.textview = (TextView) view.findViewById(R.id.textview);
                 holder.img = (MyImageView) view.findViewById(R.id.img);
                 view.setTag(holder);
-            }else{
-                holder = (ViewHolder)view.getTag();
+            } else {
+                holder = (ViewHolder) view.getTag();
             }
 
-            if(i%10 == 0){
+            if (i % 20 == 0) {
                 holder.img.setVisibility(View.VISIBLE);
                 holder.textview.setVisibility(View.GONE);
-                holder.img.setImageBitmap(mBitmap);
+                Picasso.with(inflater.getContext()).load(Data.URLS[i/10]).resize(600,400).into(holder.img);
                 holder.img.setItemPosition(i);
-
-            }else{
+            } else {
                 holder.img.setVisibility(View.GONE);
                 holder.textview.setVisibility(View.VISIBLE);
-                holder.textview.setText("position "+ i );
+                holder.textview.setText("position " + i);
             }
 
             return view;
         }
 
-        public static class ViewHolder{
+        public static class ViewHolder {
             TextView textview;
             MyImageView img;
         }
@@ -85,12 +87,11 @@ public class MyActivity extends Activity {
 
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        mListView = (ListView)findViewById(R.id.listview);
+        mListView = (ListView) findViewById(R.id.listview);
         mListView.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(AbsListView absListView, int i) {
@@ -99,31 +100,30 @@ public class MyActivity extends Activity {
 
             @Override
             public void onScroll(AbsListView absListView, int i, int i2, int i3) {
-                Log.d("Ted","i "+i+" "+i2+" "+i3);
-                EventBus.getDefault().post(new MyImageView.ChangeYEvent(i));
-            }
-        });
-
-        Picasso.with(this).load("http://i.imgur.com/zkXguhk.png").into(new Target() {
-
-            @Override
-            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                Log.d("ted","onBitmapLoaded");
-                mAdapter = new MyAdapter(MyActivity.this,bitmap);
-                mListView.setAdapter(mAdapter);
-            }
-
-            @Override
-            public void onBitmapFailed(Drawable errorDrawable) {
-                Log.d("ted","onBitmapFailed");
-            }
-
-            @Override
-            public void onPrepareLoad(Drawable placeHolderDrawable) {
-                Log.d("ted","onPrepareLoad");
+//                Log.d("Ted", "i " + i + " " + i2 + " " + i3);
+//                if(absListView.getChildAt(10) !=null) {
+//                    Log.d("Ted", "topp " + absListView.getChildAt(10).getTop());
+//                }
+                EventBus.getDefault().post(new MyImageView.ChangeYEvent(i,
+                        absListView.getFirstVisiblePosition(),
+                        absListView.getLastVisiblePosition()));
             }
         });
 
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdapter = new MyAdapter(MyActivity.this);
+        mListView.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
 }
--- ParallelListView/app/src/main/java/com/example/ted/parallellistview/MyImageView.java
+++ ParallelListView/app/src/main/java/com/example/ted/parallellistview/MyImageView.java
@@ -4,7 +4,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.text.Layout;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
 import android.widget.ImageView;
 
 import de.greenrobot.event.EventBus;
@@ -13,17 +19,22 @@ import de.greenrobot.event.EventBus;
  * Created by ted on 14/12/9.
  */
 public class MyImageView extends ImageView {
-    public static class ChangeYEvent{
+    public static class ChangeYEvent {
         int y;
+        int mFirst;
+        int mLast;
 
-        public ChangeYEvent(int y) {
+        public ChangeYEvent(int y, int firstVisible, int lastVisible) {
             this.y = y;
+            mFirst = firstVisible;
+            mLast = lastVisible;
         }
     }
-    private int y=0;
-    private Bitmap mBitmap;
+
+    private int y = 0;
     private int mItemPosition;
     private Paint paint = new Paint();
+
     public MyImageView(Context context) {
         super(context);
     }
@@ -40,49 +51,50 @@ public class MyImageView extends ImageView {
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
         EventBus.getDefault().register(this);
-
+        Log.d("Ted","onAttach");
     }
 
+
+    //Not Trigger
     @Override
     protected void onDetachedFromWindow() {
+        Log.d("Ted","onDetach");
         super.onDetachedFromWindow();
         EventBus.getDefault().unregister(this);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        paint.setStyle(Paint.Style.FILL);
-        if(Math.abs(mItemPosition -y) <=30){
-            y = y - mItemPosition;
-            if(y>0) {
-                y = (30-y) * 3;
-            }else{
-                y = (30+y) *3;
-            }
-        }else{
-            y = 0;
+        if (getDrawable() != null) {
+            getDrawable().setBounds(0, 0, getWidth(), getHeight()+400 );
+            getDrawable().draw(canvas);
+        } else {
+            super.onDraw(canvas);
         }
-
-        canvas.drawBitmap(mBitmap, 0, -200+y, paint);
     }
 
-
-    @Override
-    public void setImageBitmap(Bitmap bm) {
-        mBitmap = bm;
-    }
-
-    public void setItemPosition(int pos){
+    public void setItemPosition(int pos) {
         mItemPosition = pos;
     }
 
-    public void setY(int _y){
+    public void setY(int _y) {
         y = _y;
     }
 
-    public void onEventMainThread(ChangeYEvent event){
-        setY(event.y);
-        invalidate();
+    public void onEvent(ChangeYEvent event) {
+        if(event.mFirst <mItemPosition && event.mLast > mItemPosition) {
+
+
+            int[] location = new int[2];
+
+            getLocationInWindow(location);
+
+            Log.d("Ted", "top " + location[0] + " " + location[1]);
+            if (location[1] < 500 && location[1] > 0) {
+                Log.d("Ted", "tranlate " + (1120 - location[1]) / 3);
+                setScrollY((500 - location[1]) / 3);
+            }
+        }
+        Log.d("Ted","event "+event.mFirst +" "+mItemPosition);
     }
 }
